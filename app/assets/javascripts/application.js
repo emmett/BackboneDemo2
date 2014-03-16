da593ea1bcc673b54d80ef598588eb4b30f0cbff
Ned Ruggeri <ruggeri@self-loop.com>
2014-03-16 14:21:29 -0700

    start using Backbone; define a Todo Backbone model

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,19 +1,25 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
+// This is a manifest file that'll be compiled into application.js,
+// which will include all the files listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+// Any JavaScript/Coffee file within this directory,
+// lib/assets/javascripts, vendor/assets/javascripts, or
+// vendor/assets/javascripts of plugins, if any, can be referenced
+// here using a relative path.
 //
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
+// It's not advisable to add code directly here, but if you do, it'll
+// appear at the bottom of the compiled file.
 //
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
+// Read Sprockets README
+// (https://github.com/sstephenson/sprockets#sprockets-directives) for
+// details about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
 //= require underscore
 //= require backbone
+//= require todo
+//= require_tree ./models
+
 // We don't need turbolinks where we're going! Also, they will break
 // things in Rails 4 :-)
 // require turbolinks
---
+++ app/assets/javascripts/todo.js
@@ -0,0 +1,10 @@
+// I prefer naming the topmost namespace `App` for simplicity.
+window.App = {
+  Models: {},
+
+  initialize: function () {
+    alert("Welcome to TODOS!");
+  }
+};
+
+$(App.initialize);
