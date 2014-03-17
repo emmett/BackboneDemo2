bdf04190aac5b1cd9e688744e816d4159b5fd549
Ned Ruggeri <ruggeri@self-loop.com>
2014-03-16 18:42:22 -0700

    write Todo#comments association method

--- app/assets/javascripts/models/todo.js
+++ app/assets/javascripts/models/todo.js
@@ -1,3 +1,9 @@
 App.Models.Todo = Backbone.Model.extend({
-  urlRoot: "/api/todos"
+  urlRoot: "/api/todos",
+
+  comments: function () {
+    this._comments = this._comments ||
+      new App.Collections.TodoComments([], { todo: this });
+    return this._comments;
+  }
 });
